6569c7e8c7b3558e3b57890a57e8ba2922d1aaa8
huaqingyi <yihuaqing@talefun.com>
2020-12-23 14:31:15 +0800

    移除 class component

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 /*
- * @FilePath: /vue3-template/src/libs/index.ts
+ * @FilePath: /vue-pandora-decorators/src/index.ts
  * @Descripttion: 
  * @version: 
  * @Author: 易华青
  * @Date: 2020-12-08 15:11:26
  * @LastEditors: huaqingyi
- * @LastEditTime: 2020-12-14 16:38:37
+ * @LastEditTime: 2020-12-23 14:30:56
  * @debugger: 
  */
 import 'reflect-metadata';
 export * from './vuex';
-export * from './component';
